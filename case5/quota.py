2a39e053915d569544308f4027cd06a524b51ca5
liuyu <liuyu@openjudge.net>
2012-09-18 17:55:39 +0800

    added new example case5

---
+++ case5/quota.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import sys
+from sandbox import *
+
+class RelaxMemoryQuota(SandboxPolicy):
+    def __init__(self, sbox, limit):
+        assert(isinstance(sbox, Sandbox))
+        self.parent = sbox
+        self.soft_limit = limit / 1024
+        pass
+    def __call__(self, e, a):
+        if e.type == S_EVENT_QUOTA and e.data == S_QUOTA_MEMORY:
+            vm, vm_peak = self.parent.probe(False)['mem_info'][:2]
+            if vm_peak < self.soft_limit:
+                return self.CONT(e, a)
+            return self.ML(e, a)
+        return super(RelaxMemoryQuota, self).__call__(e, a)
+    def CONT(self, e, a):
+        return SandboxAction(S_ACTION_CONT)
+    def ML(self, e, a):
+        return SandboxAction(S_ACTION_KILL, S_RESULT_ML)
+    pass
+
+s = Sandbox("./malloc.exe", quota=dict(memory=2**21))
+s.policy=RelaxMemoryQuota(s, 2**23)
+s.run()
+
