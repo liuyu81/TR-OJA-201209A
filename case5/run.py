6bc35f08396d3928a9280dca6b15a8ce813beaf0
liuyu <liuyu@openjudge.net>
2012-09-19 19:44:21 +0800

    modified examples to be consistent with the TR

--- case5/run.py
+++ case5/run.py
@@ -2,22 +2,22 @@
 import sys
 from sandbox import *
 
-class RelaxMemoryQuota(SandboxPolicy):
+class RelaxMemoryPolicy(SandboxPolicy):
     def __init__(self, sbox, factor):
         assert(isinstance(sbox, Sandbox))
-        self.sbox = sbox
-        self.soft_limit = factor * sbox.quota[2] / 1024
+        self.parent = sbox
+        self.limit = factor * sbox.quota[2] / 1024
         pass
     def __call__(self, e, a):
         if e.type == S_EVENT_QUOTA and e.data == S_QUOTA_MEMORY:
-            vm, vm_peak = self.sbox.probe(False)['mem_info'][:2]
-            if vm_peak < self.soft_limit:
+            vm, vm_peak = self.parent.probe(False)['mem_info'][:2]
+            if vm_peak < self.limit:
                 return SandboxAction(S_ACTION_CONT)
             return SandboxAction(S_ACTION_KILL, S_RESULT_ML)
-        return super(RelaxMemoryQuota, self).__call__(e, a)
+        return super(RelaxMemoryPolicy, self).__call__(e, a)
     pass
 
 s = Sandbox("./malloc.exe", quota=dict(memory=2**22))
-s.policy=RelaxMemoryQuota(s, 2.0)
+s.policy = RelaxMemoryPolicy(s, 2.0)
 s.run()
 
--- case6/run.py
+++ case6/run.py
@@ -9,7 +9,7 @@ if machine() not in ('x86_64', 'i686'):
 symbol = dict((getattr(Sandbox, 'S_RESULT_%s' % i), i) for i in \
     ('PD', 'OK', 'RF', 'RT', 'TL', 'ML', 'OL', 'AT', 'IE', 'BP'))
 
-class SelectiveOpen(SandboxPolicy):
+class SelectiveOpenPolicy(SandboxPolicy):
     SC_open = (2, 0) if machine() == 'x86_64' else (5, 0)
     def __init__(self, sbox):
         assert(isinstance(sbox, Sandbox))
@@ -18,7 +18,7 @@ class SelectiveOpen(SandboxPolicy):
         if e.type == S_EVENT_SYSCALL:
             if (e.data, e.ext0) == self.SC_open:
                 return self.SYS_open(e, a)
-        return super(SelectiveOpen, self).__call__(e, a)
+        return super(SelectiveOpenPolicy, self).__call__(e, a)
     def SYS_open(self, e, a):
         path, mode = self.sbox.dump(T_STRING, e.ext1), e.ext2
         if path == b"./data.in" and mode == O_RDONLY:
@@ -26,8 +26,8 @@ class SelectiveOpen(SandboxPolicy):
         return SandboxAction(S_ACTION_KILL, S_RESULT_RF)
     pass
 
-s = Sandbox(["./fopen.exe", "./data.in"])
-s.policy = SelectiveOpen(s)
+s = Sandbox(["./fopen.exe", "./secret.in"])
+s.policy = SelectiveOpenPolicy(s)
 s.run()
 
 print("result: %s" % symbol.get(s.result, 'NA'))
