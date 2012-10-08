dff1edddcdb99be307fea4a258bbce98ebfe101c
liuyu <liuyu@openjudge.net>
2012-10-08 18:51:33 +0000

    multiarch predictive quota works

--- case8/run.py
+++ case8/run.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from ctypes import c_int
 from platform import system, machine
 from sandbox import *
 
@@ -9,12 +10,10 @@ if system() not in ('Linux', ) or machine() not in ('x86_64', 'i686', ):
 
 # sandbox with (partial) support for predictive out-of-quota (memory) detection
 class PredictiveQuotaSandbox(SandboxPolicy,Sandbox):
+    # result code translation table
     result_name = dict((getattr(Sandbox, 'S_RESULT_%s' % i), i) for i in \
         ('PD', 'OK', 'RF', 'RT', 'TL', 'ML', 'OL', 'AT', 'IE', 'BP'))
     # system calls for memory-allocation
-    SC_brk = (12, 0) if machine() == 'x86_64' else (45, 0)
-    SC_mmap = (9, 0) if machine() == 'x86_64' else (90, 0)
-    SC_mremap = (25, 0) if machine() == 'x86_64' else (163, 0)
     def __init__(self, *args, **kwds):
         # initalize as a polymorphic sandbox-and-policy object
         assert(not 'policy' in kwds)
@@ -22,8 +21,24 @@ class PredictiveQuotaSandbox(SandboxPolicy,Sandbox):
         Sandbox.__init__(self, *args, **kwds)
         self.policy = self
         # initialize table of system call rules
-        self.sc_table = {self.SC_brk: self.SYS_brk, self.SC_mmap: \
-            self.SYS_mmap, self.SC_mremap: self.SYS_mremap}
+        if machine() == 'x86_64':
+            # x86_64 (multiarch) system calls
+            x86_64, i686 = 0, 1
+            SC_brk = [(12, x86_64), (45, i686), ]
+            SC_mmap = [(9, x86_64), (90, i686), ]
+            SC_mmap2 = [(192, i686), ]
+            SC_mremap = [(25, x86_64), (163, i686), ]
+        else:
+            # i686 system calls
+            SC_brk = [45, ]
+            SC_mmap = [90, ]
+            SC_mmap2 = [192, ]
+            SC_mremap = [163, ]
+        self.sc_table = {}
+        for entry, handler in zip((SC_brk, SC_mmap, SC_mmap2, SC_mremap), \
+            (self.SYS_brk, self.SYS_mmap, self.SYS_mmap, self.SYS_mremap)):
+            for sc in entry:
+                self.sc_table[sc] = handler
         # initialize policy states
         self.data_seg_end = 0 # data segment end address
         self.pending_alloc = 0 # pending memory allocation (kB)
@@ -37,7 +52,7 @@ class PredictiveQuotaSandbox(SandboxPolicy,Sandbox):
     def __call__(self, e, a):
         # handle SYSCALL/SYSRET events with local rules
         if e.type in (S_EVENT_SYSCALL, S_EVENT_SYSRET):
-            sc = (e.data, e.ext0 if machine() == 'x86_64' else 0)
+            sc = (e.data, e.ext0) if machine() == 'x86_64' else e.data
             if sc in self.sc_table:
                 return self.sc_table[sc](e, a)
         # bypass other events to base class
@@ -61,9 +76,12 @@ class PredictiveQuotaSandbox(SandboxPolicy,Sandbox):
     def SYS_mmap(self, e, a):
         PROT_READ = 0x01        # <bits/mman.h>
         PROT_WRITE = 0x02       # <bits/mman.h>
+        MAP_PRIVATE = 0x02      # 
+        MAP_ANONYMOUS = 0x20    # 
         if e.type == S_EVENT_SYSCALL:
             # pending memory map area size
-            if (e.ext1, e.ext3, e.ext4) == (0, PROT_READ | PROT_WRITE, -1):
+            if (e.ext1, e.ext3, e.ext4, c_int(e.ext5).value, e.ext6) == \
+                (0, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0):
                 return self.MEM_check(e, a, e.ext2)
             return self._KILL_RF(e, a)
         return self.MEM_check(e, a)
@@ -86,3 +104,4 @@ if __name__ == '__main__':
     s.run()
     print("result: %(result)s\ncpu: %(cpu)dms" % s.probe())
     print("mem: %dkB / %dkB" % s.probe()['mem'])
+
