f63ad73e36f87e50b0bfdf8fe14317eda3c9f7c3
liuyu <liuyu@openjudge.net>
2012-10-08 11:18:09 +0000

    revised predictive policy for readability

--- case8/run.py
+++ case8/run.py
@@ -10,57 +10,54 @@ symbol = dict((getattr(Sandbox, 'S_RESULT_%s' % i), i) for i in \
     ('PD', 'OK', 'RF', 'RT', 'TL', 'ML', 'OL', 'AT', 'IE', 'BP'))
 
 class PredictiveQuotaPolicy(SandboxPolicy):
-    # Linux system calls
+    # system calls for memory-allocation
     SC_brk = (12, 0) if arch() == 'x86_64' else (45, 0)
     SC_mmap = (9, 0) if arch() == 'x86_64' else (90, 0)
     SC_mremap = (25, 0) if arch() == 'x86_64' else (163, 0)
-    # pre-defined actions
-    CONT = SandboxAction(S_ACTION_CONT)
-    KILL_ML = SandboxAction(S_ACTION_KILL, S_RESULT_ML)
-    KILL_RF = SandboxAction(S_ACTION_KILL, S_RESULT_RF)
     def __init__(self, sbox):
         assert(isinstance(sbox, Sandbox))
         self.sbox = sbox
-        self.sc_table = {\
-            self.SC_brk: self.SYS_brk,
-            self.SC_mmap: self.SYS_mmap,
-            self.SC_mremap: self.SYS_mremap,
-        }
-        self.data_segment = 0
-        self.pending_alloc = 0
-    @property
-    def mem(self):
-        return self.sbox.probe()['mem_info'][0] * 1024, self.pending_alloc
+        self.data_segment_end = 0 # data segment end address
+        self.pending_alloc = 0 # pending memory allocation in byte
+        self.sc_table = {self.SC_brk: self.SYS_brk, \
+            self.SC_mmap: self.SYS_mmap, self.SC_mremap: self.SYS_mremap}
+        pass
     def __call__(self, e, a):
         if e.type in (S_EVENT_SYSCALL, S_EVENT_SYSRET):
             sc = (e.data, e.ext0 if arch() == 'x86_64' else 0)
             if sc in self.sc_table:
                 return self.sc_table[sc](e, a)
         return super(PredictiveQuotaPolicy, self).__call__(e, a)
+    @property
+    def mem(self):
+        return (self.sbox.probe()['mem_info'][0] * 1024, self.pending_alloc)
     def MEM_audit(self, e, a, incr=0):
         if sum(self.mem) + incr > self.sbox.quota[2]:
             self.pending_alloc = incr
-            return self.KILL_ML
-        return self.CONT
+            return SandboxAction(S_ACTION_KILL, S_RESULT_ML)
+        return SandboxAction(S_ACTION_CONT)
     def SYS_brk(self, e, a):
-        if e.type == S_EVENT_SYSCALL and e.ext1 > 0:
-            # data segment increment
-            incr = e.ext1 - self.data_segment
-            return self.MEM_audit(e, a, incr)
-        elif e.type == S_EVENT_SYSRET:
-            self.data_segment = e.ext1
-        return self.CONT
+        if e.type == S_EVENT_SYSCALL:
+            # pending data segment increment
+            if e.ext1 > 0:
+                incr = e.ext1 - self.data_segment_end
+                return self.MEM_audit(e, a, incr)
+        else:
+            # update data segment end address
+            self.data_segment_end = e.ext1
+        return SandboxAction(S_ACTION_CONT)
     def SYS_mmap(self, e, a):
         PROT_READ = 0x01        # <bits/mman.h>
         PROT_WRITE = 0x02       # <bits/mman.h>
         if e.type == S_EVENT_SYSCALL:
-            # memory map area size
-            if e.ext3 != PROT_READ | PROT_WRITE or e.ext4 != -1:
-                return self.KILL_RF
-            return self.MEM_audit(e, a, e.ext2)
-        return self.CONT
+            # pending memory map area size
+            if (e.ext1, e.ext3, e.ext4) == (0, PROT_READ | PROT_WRITE, -1):
+                return self.MEM_audit(e, a, e.ext2)
+            return SandboxAction(S_ACTION_KILL, S_RESULT_RF)
+        return SandboxAction(S_ACTION_CONT)
     def SYS_mremap(self, e, a):
-        return self.KILL_RF
+        # 
+        return SandboxAction(S_ACTION_KILL, S_RESULT_RF)
     pass
 
 s = Sandbox("./malloc.exe", quota=dict(memory=2**22))
