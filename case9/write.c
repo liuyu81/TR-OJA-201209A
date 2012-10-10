65f8120aaec5c4bb3c76524be4b286ac92aaebaf
LIU Yu <liuyu@openjudge.net>
2012-10-10 20:42:09 +0800

    updated header date

--- case9/write.c
+++ case9/write.c
@@ -1,7 +1,7 @@
 /**
  * @copyright (c) 2012, OpenJudge Alliance <http://openjudge.net>
  * @author LIU Yu <pineapple.liu@gmail.com>
- * @date 2012/04/30
+ * @date 2012/10/10
  * 
  * This program source code is part of the OpenJudge Alliance Technical Report 
  * (TR-OJA-201209A) at <http://openjudge.net/TR/201209A>.
